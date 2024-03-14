04368383a603fbbb15c6111796f6c63bca52d0b2
Julie0411 <150553151+Julie0411@users.noreply.github.com>
2024-03-14 14:34:23 +0100

    My first commit that contain all the series that I do until now with Genazzi

--- tutorial/for_tutorial09.java
+++ tutorial/for_tutorial09.java
@@ -3,7 +3,25 @@ package tutorial;
 public class for_tutorial09 {
 
     public static void main(String[] args) {
+
+        /*
+        for (int i = 1; i <= 10; i++) {
+           System.out.println(i);
+        }
         
+        Nella parentesi tonda del for ci sono sempre tre settori che sono 
+        separate da ';'
+        Il primo settore è la creazione dell'iteratore
+        Il secondo settore è la condizione
+        Il terzo settore è l'incrementatore
+        Il for è più comodo rispetto al while, questo perché ha un codice 
+        ottimizzato e più pulito.
+        */
+        
+        for (int i = 0; i <= 70; i += 7) {
+            System.out.println(i);
+        }
+
     }
 
 }
