cdef24a9bba9a5eeab4aefc3421e6d446cf267d7
Julie0411 <150553151+Julie0411@users.noreply.github.com>
2024-03-27 15:04:18 +0100

    I began the serie10 that is about the for

---
+++ serie10/Esercizio06.java
@@ -0,0 +1,15 @@
+package serie10;
+
+public class Esercizio06 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                System.out.println(i + "-" + j);
+            }
+        }
+        
+    }
+
+}
